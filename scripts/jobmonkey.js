e5bb3c6caf40073ab7e6fed5b12605f565bc74ea
Myles Williams <mwilliams.sfo@gmail.com>
2025-04-19 15:26:44 -0700

    Filter jobs by title

--- scripts/jobmonkey.js
+++ scripts/jobmonkey.js
@@ -3,6 +3,9 @@ const selectors = {
   feed: 'div[data-finite-scroll-hotkey-context=FEED]',
   feedItemHeaderText: '.update-components-header .update-components-header__text-view',
 
+  jobList: '.jobs-search-two-pane__layout .scaffold-layout__list > div > ul',
+  jobTitle: '.job-card-list__title--link strong',
+
   jobDetails: '.jobs-search__job-details',
   jobDetailsModule: '.job-details-module',
   fitLevelCard: '.job-details-fit-level-card',
@@ -10,17 +13,46 @@ const selectors = {
   upsellPremiumContainer: '.upsell-premium-custom-section-card__container',
 };
 
-const setGone = elt => { elt.classList.toggle('jm-gone', true); }
+const setGone = (elt, gone) => { elt.classList.toggle('jm-gone', gone); };
+
+const setHidden = (elt, hidden) => {
+  elt.classList.toggle('jm-hidden', hidden);
+};
 
 const isSuggestedPost = feedItem =>
-  feedItem.querySelector(selectors.feedItemHeaderText)
-    ?.textContent?.trim() === 'Suggested';
+  feedItem
+    .querySelector(selectors.feedItemHeaderText)
+    ?.textContent
+    ?.trim() === 'Suggested';
 
 const hideSuggestedPosts = feed => {
   for (const child of feed.childNodes) {
     if (child.nodeType != Node.ELEMENT_NODE) continue;
     if (child.tagName == 'DIV' && isSuggestedPost(child)) {
-      setGone(child);
+      setGone(child, true);
+    }
+  }
+};
+
+const isInterestingTitle = title => {
+  title = title.toLowerCase();
+  if (title.match(/\b(?:manager|principal|lead|test|qa)\b/)) return false;
+
+  return true;
+};
+
+const isInterestingJob = job => {
+  const title = job.querySelector(selectors.jobTitle)?.textContent?.trim();
+  if (title && !isInterestingTitle(title)) return false;
+
+  return true;
+};
+
+const scrubJobList = list => {
+  for (const child of list.childNodes) {
+    if (child.nodeType != Node.ELEMENT_NODE) continue;
+    if (child.tagName == 'LI') {
+      setHidden(child, !isInterestingJob(child));
     }
   }
 };
@@ -28,16 +60,16 @@ const hideSuggestedPosts = feed => {
 const scrubJobDetails = details => {
   for (const module of details.querySelectorAll(selectors.jobDetailsModule)) {
     if (module.querySelector(selectors.fitLevelCard)) {
-      setGone(module);
+      setGone(module, true);
     }
   }
   const upsell = details.querySelector(selectors.upsellPremiumContainer)
   if (upsell) {
-    setGone(upsell);
+    setGone(upsell, true);
   }
 };
 
-let feedObserver = null, jobDetailsObserver = null;
+let feedObserver = null, jobListObserver = null, jobDetailsObserver = null;
 
 const bodyObserver = new MutationObserver((mutationList, observer) => {
   const feed = document.querySelector(selectors.feed);
@@ -53,6 +85,21 @@ const bodyObserver = new MutationObserver((mutationList, observer) => {
   } else if (!feed && feedObserver) {
     feedObserver.disconnect();
     feedObserver = null;
+  }
+
+  const jobList = document.querySelector(selectors.jobList);
+  if (jobList && !jobListObserver) {
+    jobListObserver = new MutationObserver((mutationList, observer) => {
+      scrubJobList(jobList);
+    });
+    jobListObserver.observe(jobList, {
+      childList: true,
+      attributes: true,
+      subtree: true,
+    });
+  } else if (!jobList && jobListObserver) {
+    jobListObserver.disconnect();
+    jobListObserver = null;
   }
 
   const details = document.querySelector(selectors.jobDetails);
