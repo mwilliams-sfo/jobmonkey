30af136c866f93c5e58b936158e593e2969321b8
Myles Williams <mwilliams.sfo@gmail.com>
2025-04-18 23:30:48 -0700

    Use scoped MutationObservers

--- scripts/jobmonkey.js
+++ scripts/jobmonkey.js
@@ -1,6 +1,6 @@
 
 const selectors = {
-  feedItem: 'div[data-finite-scroll-hotkey-context=FEED] > div',
+  feed: 'div[data-finite-scroll-hotkey-context=FEED]',
   feedItemHeaderText: '.update-components-header .update-components-header__text-view',
 
   jobDetails: '.jobs-search__job-details',
@@ -10,20 +10,34 @@ const selectors = {
 const isSuggestedPost = feedItem =>
   feedItem.querySelector(selectors.feedItemHeaderText)?.textContent?.trim() === 'Suggested';
 
-const hideSuggestedPosts = () => {
-  for (const item of document.querySelectorAll(selectors.feedItem)) {
-    if (isSuggestedPost(item)) {
-      item.classList.toggle('jm-gone', true);
+const hideSuggestedPosts = feed => {
+  for (const child of feed.childNodes) {
+    if (child.nodeType != Node.ELEMENT_NODE) continue;
+    if (child.tagName == 'DIV' && isSuggestedPost(child)) {
+      child.classList.toggle('jm-gone', true);
     }
   }
 };
 
-const observer = new MutationObserver((mutationList, observer) => {
-  if (window.location.pathname === '/feed/') {
-    hideSuggestedPosts();
+let feedObserver = null;
+
+const bodyObserver = new MutationObserver((mutationList, observer) => {
+  const feed = document.querySelector(selectors.feed);
+  if (feed && !feedObserver) {
+    feedObserver = new MutationObserver((mutationList, observer) => {
+      hideSuggestedPosts(feed);
+    });
+    feedObserver.observe(feed, {
+      childList: true,
+      attributes: true,
+      subtree: true,
+    });
+  } else if (!feed && feedObserver) {
+    feedObserver.disconnect();
+    feedObserver = null;
   }
 });
-observer.observe(document.body, {
+bodyObserver.observe(document.body, {
   childList: true,
   attributes: true,
   subtree: true,
