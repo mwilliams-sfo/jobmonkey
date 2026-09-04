4dee1cb5af8ea3edf8069d4993f54d1b6fb4bee5
Myles Williams <mwilliams.sfo@gmail.com>
2026-09-03 17:38:45 -0700

    Use esbuild to package extension

---
+++ build.js
@@ -0,0 +1,9 @@
+import * as esbuild from 'esbuild';
+import fs from 'fs';
+
+await esbuild.build({
+  entryPoints: ['src/content/linkedin.js'],
+  bundle: true,
+  outdir: 'dist/content',
+});
+await fs.promises.cp('public', 'dist', {recursive: true});
